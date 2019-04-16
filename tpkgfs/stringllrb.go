42554cdb8fbc2847db4b2561a187778783e85718
Mark Karpeles <magicaltux@gmail.com>
2019-04-16 15:49:38 +0900

    forgot file

---
+++ tpkgfs/stringllrb.go
@@ -0,0 +1,16 @@
+package tpkgfs
+
+import (
+	"strings"
+
+	"github.com/petar/GoLLRB/llrb"
+)
+
+type llrbString struct {
+	k string
+	v uint64
+}
+
+func (s *llrbString) Less(than llrb.Item) bool {
+	return strings.Compare(s.k, than.(*llrbString).k) < 0
+}
