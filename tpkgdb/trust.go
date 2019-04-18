d321e169e763788283953a9956fcada901243579
Mark Karpeles <magicaltux@gmail.com>
2019-04-18 18:12:51 +0900

    add trust db

---
+++ tpkgdb/trust.go
@@ -0,0 +1,13 @@
+package tpkgdb
+
+// Trusted DB signatories
+var trustedDbSig = map[string]string{
+	"dbXFt7yjQhHcpPq0UIRMonWUyO5f4DoNVOYcefKCIhU": "Mark Karpeles <magicaltux@gmail.com>",
+	"TPDBy98_HUS9JikdsjCbw_FjUjNWTx5ryNron_DHEBA": "TPDB emergency key #1",
+}
+
+var trustedPkgSig = map[string]string{
+	"n21_CiuzEuo_OaSfEVodAXpQtcc4Qe_NprMSAs3B9QU": "Mark Karpeles <magicaltux@gmail.com>",
+	"TPKGBiS_JH8tnSUgAE-4_f8gOkvzFqR2dnOKrJyLRus": "TPKG emergency key #1",
+	"TPKG37jwLgVabYPH3GSDAqSoAxlRAHN-aTQT3dLYLMQ": "TPKG emergency key #2",
+}
